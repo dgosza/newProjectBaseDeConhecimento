a32cc1129631a0ae0b5b074ecaed8907d78b87d3
diego <diegomagasouza@gmail.com>
2019-03-02 20:09:35 -0300

    first commit

---
+++ acessoTecnico/view/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Acesso Técnico</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!--------------------FAV ICON--------------------------------------------!-->
+    <link rel="icon" type="image/png" sizes="96x96" href="imgs/favBase.png">
+
+    <!--------------------MATERIALIZE FRAMEWORK AND CSS STYLE------------------!-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css">
+    <link rel="stylesheet" type="text/css" media="screen" href="indexTipoAcesso/indexTipoAcesso.css" />
+
+    <!--------------------ICONES MATERIALIZE FRAMEWORK-----------------------!-->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+</head>
+<body>
+
+    <div class="row container">
+        
+
+    </div>
+    
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Base de Conhecimento</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!--------------------FAV ICON--------------------------------------------!-->
+    <link rel="icon" type="image/png" sizes="96x96" href="indexTipoAcesso/favBase.png">
+
+    <!--------------------MATERIALIZE FRAMEWORK AND CSS STYLE------------------!-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css">
+    <link rel="stylesheet" type="text/css" media="screen" href="indexTipoAcesso/indexTipoAcesso.css" />
+
+    <!--------------------ICONES MATERIALIZE FRAMEWORK-----------------------!-->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+</head>
+<body>
+
+    <div class="row container escolheAcesso">
+        <div class="col s12 l12">
+            
+            <div class="col s12 l12">
+                <br><br><br><br><br>
+                <img class="responsive-img" src="indexTipoAcesso/logoPreventBranco.png"> 
+            </div>
+            
+        </div>
+
+        <div class="col s12 l12">
+            <br><br><br>
+            <h6>Escolha o tipo de acesso a Base De Conhecimento da Prevent Senior</h6>
+            <a class="waves-effect waves-light btn white" style="color:#0d47a1;" href="acessoTecnico/view/index.php">Acesso Técnico TI</a>
+            <a class="waves-effect waves-light btn white" style="color:#0d47a1;">Acesso Usuário</a>
+        </div>
+
+    </div>
+
+    <footer>
+
+    </footer>
+    
+</body>
+</html>
