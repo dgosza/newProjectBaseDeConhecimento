2922877d38418e9f30aa87ea3d37f6333c02a769
diego <diegomagasouza@gmail.com>
2019-03-03 15:11:26 -0300

    update meuperfil, acessoUsuario and acessoTecnico pages

---
+++ acessoUsuario/view/index.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Acesso Usuário</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!--------------------FAV ICON--------------------------------------------!-->
+    <link rel="icon" type="image/png" sizes="96x96" href="imgs/favBase.png">
+
+    <!--------------------MATERIALIZE FRAMEWORK AND CSS STYLE------------------!-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css">
+    <link rel="stylesheet" type="text/css" media="screen" href="css/style.css" />
+
+    <!--------------------ICONES MATERIALIZE FRAMEWORK-----------------------!-->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+   <!--------------------ICONES FONT_AWESOME --------------------------------!-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Staatliches" rel="stylesheet">
+
+</head>
+<body>
+
+    <!----------------------------------NAV BAR--------------------------->
+   
+
+    <div class="middle">
+        <div class="row container">
+
+            <div class="col s12 l12 departamentos">
+
+                <div class="col s12 l12">
+                    <h1 class="flow-text white-text">Escolha o seu Departamento</h1><br><br><br><br>
+                </div> 
+
+                <div class="col s6 l2 center" style="color:#fff">
+                    <i class="fa fa-paint-brush fa-5x indicacao" style="color: #fff"></i>
+                    <h5 class="flow-text">Marketing</h5>
+                </div>
+                
+                <div class="col s6 l2 center" style="color:#fff">
+                    <i class="fa fa-bank fa-5x indicacao" style="color: #fff"></i>
+                    <h5 class="flow-text">Jurídico</h5>
+                </div> 
+
+                <div class="col s6 l2 center" style="color:#fff">
+                    <i class="fa fa-sellsy fa-5x indicacao" style="color: #fff"></i>
+                    <h5 class="flow-text">Vendas</h5>
+                </div> 
+
+                <div class="col s6 l2 center" style="color:#fff">
+                    <i class="fa fa-money fa-5x indicacao" style="color: #fff"></i>
+                    <h5 class="flow-text">Financeiro</h5>
+                </div> 
+
+                <div class="col s6 l2 center" style="color:#fff">
+                    <i class="fa fa-address-card-o fa-5x indicacao" style="color: #fff"></i>
+                    <h5 class="flow-text">Departamento Pessoal / RH</h5>
+                </div> 
+
+                <div class="col s6 l2 center" style="color:#fff">
+                    <i class="fa fa-medkit fa-5x indicacao" style="color: #fff"></i>
+                    <h5 class="flow-text">Contas Médicas</h5>
+                </div>             
+                                
+            </div>
+
+            <div class="col s12 l12 departamentos">
+                <div class="col s6 l2 center" style="color:#fff">
+                    <i class="fa fa-building fa-5x indicacao" style="color: #fff"></i>
+                    <h5 class="flow-text">Infraestrutura</h5>
+                </div> 
+            </div>
+
+
+
+            
+
+            <div class="col s12 l12">
+                <br><br><br>
+                <a href="../../index.php" style="color:#fff;" class="left"><i class="material-icons left">keyboard_backspace</i>Voltar ao Menu Principal</a>
+            </div>
+
+        </div>
+    </div>
+
+    <!----------------------------------FOOTER--------------------------->
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script>
+    <script src="fonte.js"></script>
+    
+    <!--------------------SCRIPTS DE INICIALIZAÇÂO DO FRAMEWORK-----------------------!-->
+    
+</body>
+</html>
